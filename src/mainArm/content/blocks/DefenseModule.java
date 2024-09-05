18d40bf5f6e671e346d4d020c7d4f7bcccc24e38
Mochamad Rizky <mochamadrizky.dev@gmail.com>
2024-09-06 06:37:43 +0700

    Add new two turrets and fix some bullet that cannot shot ground enemies.

--- src/mainArm/content/blocks/DefenseModule.java
+++ src/mainArm/content/blocks/DefenseModule.java
@@ -2,6 +2,7 @@ package mainArm.content.blocks;
 
 import arc.graphics.Color;
 import mindustry.content.*;
+import mindustry.entities.Effect;
 import mindustry.entities.bullet.*;
 import mindustry.entities.effect.*;
 import mindustry.entities.part.RegionPart;
@@ -11,6 +12,7 @@ import mindustry.graphics.Pal;
 import mindustry.type.Category;
 import mindustry.world.Block;
 import mindustry.world.blocks.defense.turrets.*;
+import mindustry.world.consumers.ConsumeLiquid;
 import mindustry.world.draw.DrawTurret;
 
 import static mindustry.type.ItemStack.*;
@@ -19,7 +21,7 @@ public class DefenseModule {
     public static Block
 
     //Turrets
-    gunmachina, mightydao;
+    gunmachina, mightydao, rapidwings, gatlingwings;
 
     public static void load() {
 
@@ -33,6 +35,7 @@ public class DefenseModule {
                        height = 8f;
                        knockback = 0.3f;
                        reloadMultiplier = 1.7f;
+                       collidesGround = true;
                    }},
                    Items.pyratite, new FlakBulletType(7f, 15){{
                        shootEffect = Fx.shootSmall;
@@ -207,10 +210,192 @@ public class DefenseModule {
             range = 460;
             shootCone = 20f;
             scaledHealth = 260;
-            rotateSpeed = 7f;
+            rotateSpeed = 5f;
             researchCostMultiplier = 0.04f;
 
             limitRange(9f);
+        }};
+
+        rapidwings = new ItemTurret("rapidwings") {{
+            requirements(Category.turret, with(Items.copper, 300, Items.lead, 300, Items.silicon, 150, Items.graphite, 250));
+
+            Effect sfe = new MultiEffect(Fx.shootBigColor, Fx.colorSparkBig);
+
+            ammo(
+                    Items.graphite, new BasicBulletType(7.5f, 15){{
+                        width = 12f;
+                        hitSize = 7f;
+                        height = 20f;
+                        shootEffect = sfe;
+                        smokeEffect = Fx.shootBigSmoke;
+                        ammoMultiplier = 4;
+                        pierceCap = 2;
+                        pierce = true;
+                        pierceBuilding = true;
+                        hitColor = backColor = trailColor = Pal.berylShot;
+                        frontColor = Color.white;
+                        trailWidth = 2.1f;
+                        trailLength = 10;
+                        hitEffect = despawnEffect = Fx.hitBulletColor;
+                        buildingDamageMultiplier = 0.3f;
+                    }},
+                    Items.titanium, new BasicBulletType(8f, 30){{
+                        width = 13f;
+                        height = 19f;
+                        hitSize = 7f;
+                        shootEffect = sfe;
+                        smokeEffect = Fx.shootBigSmoke;
+                        ammoMultiplier = 8;
+                        reloadMultiplier = 1f;
+                        pierceCap = 3;
+                        pierce = true;
+                        pierceBuilding = true;
+                        hitColor = backColor = trailColor = Pal.tungstenShot;
+                        frontColor = Color.white;
+                        trailWidth = 2.2f;
+                        trailLength = 11;
+                        hitEffect = despawnEffect = Fx.hitBulletColor;
+                        rangeChange = 40f;
+                        buildingDamageMultiplier = 0.85f;
+                    }}
+            );
+            shoot = new ShootAlternate(8f) {{
+                shotDelay = 0;
+            }};
+
+            recoils = 2;
+            drawer = new DrawTurret(){{
+                parts.add(new RegionPart("-barrels"){{
+                    progress = PartProgress.recoil;
+                    recoilIndex = 0;
+                    under = true;
+                    moveY = -1.5f;
+                }});
+            }};
+
+            shootSound = Sounds.shootAlt;
+            outlineColor = Pal.darkOutline;
+            size = 3;
+            reload = 10f;
+            recoilTime = reload * 2f;
+            coolantMultiplier = 0.5f;
+            recoil = 3f;
+            range = 260f;
+            inaccuracy = 1.5f;
+            shootCone = 10f;
+            scaledHealth = 280;
+            rotateSpeed = 7.5f;
+        }};
+
+        gatlingwings = new ItemTurret("gatlingwings") {{
+            requirements(Category.turret, with(Items.titanium, 650, Items.silicon, 150, Items.thorium, 50, Items.plastanium, 100));
+
+            ammo(
+                    Items.titanium, new BasicBulletType(){{
+                        damage = 35;
+                        speed = 7f;
+                        width = height = 12;
+                        shrinkY = 0.2f;
+                        backSprite = "large-bomb-back";
+                        sprite = "mine-bullet";
+                        velocityRnd = 0.11f;
+                        collidesGround = true;
+                        collidesTiles = false;
+                        shootEffect = Fx.shootBig2;
+                        smokeEffect = Fx.shootSmokeDisperse;
+                        frontColor = Color.white;
+                        backColor = trailColor = hitColor = Color.blue;
+                        trailChance = 0.44f;
+                        ammoMultiplier = 3f;
+
+                        lifetime = 64f;
+                        rotationOffset = 90f;
+                        trailRotation = true;
+                        trailEffect = Fx.disperseTrail;
+
+                        hitEffect = despawnEffect = Fx.hitBulletColor;
+                    }},
+                    Items.surgeAlloy, new BasicBulletType(){{
+                        damage = 65;
+                        speed = 8.5f;
+                        width = height = 16;
+                        shrinkY = 0.3f;
+                        backSprite = "large-bomb-back";
+                        sprite = "mine-bullet";
+                        velocityRnd = 0.11f;
+                        collidesGround = true;
+                        collidesTiles = false;
+                        shootEffect = Fx.shootBig2;
+                        smokeEffect = Fx.shootSmokeDisperse;
+                        frontColor = Color.white;
+                        backColor = trailColor = hitColor = Pal.surge;
+                        trailChance = 0.44f;
+                        ammoMultiplier = 9f;
+
+                        lifetime = 64f;
+                        rotationOffset = 90f;
+                        trailRotation = true;
+                        trailEffect = Fx.disperseTrail;
+
+                        hitEffect = despawnEffect = Fx.hitBulletColor;
+                    }}
+            );
+            reload = 9f;
+            shootY = 15f;
+            rotateSpeed = 5f;
+            shootCone = 30f;
+            consumeAmmoOnce = true;
+            shootSound = Sounds.shootBig;
+
+            drawer = new DrawTurret("reinforced-"){{
+                parts.add(new RegionPart("-side"){{
+                              mirror = true;
+                              under = true;
+                              moveX = 1.75f;
+                              moveY = -0.5f;
+                          }},
+                        new RegionPart("-top"){{
+                            under = false;
+                            moveY = -1.5f;
+                            progress = PartProgress.recoil;
+                            heatProgress = PartProgress.recoil.add(0.25f).min(PartProgress.warmup);
+                            heatColor = Color.sky.cpy().a(0.9f);
+                        }},
+                        new RegionPart("-blade"){{
+                            heatProgress = PartProgress.warmup;
+                            heatColor = Color.sky.cpy().a(0.9f);
+                            mirror = true;
+                            under = true;
+                            moveY = 1f;
+                            moveX = 1.5f;
+                            moveRot = 8;
+                        }},
+                        new RegionPart("-mid"){{
+                            under = true;
+                        }});
+            }};
+
+            shoot = new ShootAlternate(){{
+                spread = 4.7f;
+                shots = 4;
+                barrels = 4;
+            }};
+
+            targetGround = true;
+            inaccuracy = 3f;
+
+            shootWarmupSpeed = 0.08f;
+
+            outlineColor = Pal.darkOutline;
+
+            scaledHealth = 280;
+            range = 390f;
+            size = 4;
+
+            coolant = consume(new ConsumeLiquid(Liquids.water, 20f / 60f));
+            coolantMultiplier = 2.5f;
+
+            limitRange(5f);
         }};
     }
 }
